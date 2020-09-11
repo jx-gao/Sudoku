79ab9a10fd6ee868c85496d3d7619d9db748436c
mushi-jx <jiaxinabecgao@gmail.com>
2020-09-11 15:48:24 +0200

    Timer with terminal

--- bfs.cpp
+++ bfs.cpp
@@ -192,6 +192,10 @@ int main(){
 
     fill(grid);
 
+
+    time_t start,end;
+    time(&start);
+
     bool sol = solve(grid);
     if(sol == true){
         print(grid);
@@ -199,6 +203,12 @@ int main(){
         cout << "No Solution" << endl;
     }
 
+    time(&end);
+
+    double time_taken = double(end - start); 
+    cout << "Time taken by program is : " << fixed 
+         << time_taken; 
+    cout << " sec " << endl; 
 
     return 0;
 }
